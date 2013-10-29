016896bce858c5e428b6a34fe27f03b24b23f025
Bonnie <bonnadinho@hotmail.com>
2013-10-29 17:20:14 +0100

    changes

--- src/ObserverInterfaces/ObserverInterface.java
+++ src/ObserverInterfaces/ObserverInterface.java
@@ -7,6 +7,8 @@ public interface  ObserverInterface
 {	
 	public void registerObserver(PersonView observable);
 	public void registerObserver(AccountView observable);
+	
+	
 	public void notifyObserver();
 	public void removeObserver(ObservableInterface observable);	
 }
